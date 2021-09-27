ec8920b1d9ebf74d7bd5469d87be7e469cfe1765
la-mer-elen <m.elena09109@gmail.com>
2021-09-27 13:27:17 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const board = document.querySelector('#board')
+const colors = ['#ADFF2F', '#F08080', '#C71585', '#FF7F50', '#FFFF00', '#E6E6FA', '#DA70D6', '#4B0082', '#7FFFD4', '#800000']
+const SQUARES_NUMBER = 500
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', setColor)
+
+    square.addEventListener('mouseleave', removeColor)
+
+    board.append(square)
+}
+
+function setColor(event) {
+    const element = event.target
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px #FFD700`
+}
+
+function removeColor(event) {
+    const element = event.target
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)]
+}
+
